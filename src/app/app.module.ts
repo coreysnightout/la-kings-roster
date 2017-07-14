0bd636c01335fd77c1157b8f0ee0b7565019bf75
corey redding <coreysnightout@gmail.com>
2017-07-14 13:50:01 -0700

    add welcome component and about component, linking them to the app.routing.ts as well as the app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,45 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+// Routing
+import { routing } from './app.routing';
+// components
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.welcome.component';
+// import { MarketPlaceComponent } from './market-place/market-place.component';
+// import { AdminComponent } from './admin/admin.component';
+// import { ProductDetailComponent } from './product-detail/product-detail.component';
+// import { EditProductComponent } from './edit-product/edit-product.component';
+// // database
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    AboutComponent
+    // MarketPlaceComponent,
+    // AdminComponent,
+    // ProductDetailComponent,
+    // EditProductComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
